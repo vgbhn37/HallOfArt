445096431ddf13abc354d7301bd887bf6ae19753
hwangbj123 <bj09289612@gmail.com>
2023-09-25 15:53:11 +0900

    admin page

---
+++ src/main/java/com/silver/hallofart/controller/ErrorController.java
@@ -0,0 +1,21 @@
+package com.silver.hallofart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+	@GetMapping("/401")
+	public String error401() {
+		return "401";
+	}
+	@GetMapping("/404")
+	public String error404() {
+		return "404";
+	}
+	@GetMapping("/500")
+	public String error500() {
+		return "500";
+	}
+}
