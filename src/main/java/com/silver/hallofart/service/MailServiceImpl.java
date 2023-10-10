e07ee7da629025040950b60e68fce61e89bd26de
hwangbj123 <bj09289612@gmail.com>
2023-10-10 10:27:20 +0900

    mail

---
+++ src/main/java/com/silver/hallofart/service/MailServiceImpl.java
@@ -0,0 +1,20 @@
+package com.silver.hallofart.service;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public interface MailServiceImpl {
+
+	// 메일 내용 작성
+	MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException;
+	
+
+	// 랜덤 인증 코드 전송
+	String createKey();
+
+	// 메일 발송
+	String sendSimpleMessage(String to) throws Exception;
+	
+}
