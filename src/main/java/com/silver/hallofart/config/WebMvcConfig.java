0631a6911ab5548e2a63a9b159820a766e18902c
hjm8333 <35636560+hjm8333@users.noreply.github.com>
2023-10-11 12:00:07 +0900

    인터셉터 추가
    payment/inquery/booking 등록함

---
+++ src/main/java/com/silver/hallofart/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.silver.hallofart.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private AuthInterceptor authInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor).addPathPatterns("/booking/**").addPathPatterns("/payment/**").addPathPatterns("/inquery/**");
+	}
+	
+}
