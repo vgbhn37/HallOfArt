68b8cece5b1cedf8fcc3af3c25eab90d6eb0792e
hjm8333 <hjm8333@naver.com>
2023-10-16 00:13:10 +0900

    메인 레이아웃 생성

---
+++ src/main/java/com/silver/hallofart/controller/MainController.java
@@ -0,0 +1,54 @@
+package com.silver.hallofart.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @Autowired
+    private HttpSession session;
+    @GetMapping({"/main", "/"})
+    public String main() {
+        log.info("메인 페이지 컨트롤러 실행");
+
+//        List<Show> showsList = mainService.readShowDto();
+//        List<Announcement> selectAnnouncement = mainService.selectAnnouncement();
+//        model.addAttribute("selectAnnouncement", selectAnnouncement);
+//        User principal = (User) session.getAttribute(Define.PRINCIPAL);
+//        if (principal == null) {
+//            List<RequestShowDto> lists = scheduleService.readShowSchedule();
+//            model.addAttribute("lists", lists);
+//            model.addAttribute("principal", null);
+//        } else if (principal.getRoleId() == 1) {
+//            List<RequestShowDto> lists = scheduleService.readShowSchedule();
+//            model.addAttribute("lists", lists);
+//        }else {
+//            List<Notice> noticeList = noticeService.readNotice(principal.getId());
+//            List<RequestShowDto> showList = scheduleService.readMyShowSchedule(principal.getId());
+//            model.addAttribute("lists", showList);
+//            if (noticeList == null || noticeList.size() == 0) {
+//                model.addAttribute("noticeList", null);
+//                model.addAttribute("message", 0);
+//            } else {
+//                model.addAttribute("noticeList", noticeList);
+//            }
+//            model.addAttribute("principal", principal);
+//        }
+//        if (showsList.isEmpty()) {
+//            model.addAttribute("showsList", null);
+//        } else {
+//            model.addAttribute("showsList", showsList);
+//        }
+
+        return "main";
+    }
+
+}
